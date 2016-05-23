3904ebd1e0d42225bda980a15ced2ed94bbcd9f8
uenal akkaya <uenal.akkaya@gmail.com>
2016-05-23 23:45:20 +0200

    Analyzed Data: min and max values of x,y,z of acc and gyr first implementation of data pruning and reduction (dataModel2.py).

--- main/main.py
+++ main/main.py
@@ -4,12 +4,12 @@ Created on 08.05.2016
 @author: freddy
 '''
 
-from atml.main.dataModel import dataModel
+from dataModel2 import dataModel2
 
 #main file, the interpreter should start here
 
 #read in and construct the data model
-model = dataModel()
+model = dataModel2()
 
 # TODO: classifiers
    
